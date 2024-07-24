d2ef2e2db2c5b36dbed74b303005489186dfd938
jaythan <yegarrick12@gmail.com>
2024-07-24 10:29:22 +0800

    -

--- src/app/components/LogoSection.tsx
+++ src/app/components/LogoSection.tsx
@@ -3,11 +3,125 @@ import IconTitle from "./iconTitle";
 
 const LogoSection = () => {
   return (
-    <div className="mt-24 py-4 bg-white">
+    <div className="mt-24 py-4">
       <section id="logo">
         <div className="pt-2 pb-2">
-          <div className="flex overflow-hidden space-x-18 mt-4">
-            <div className="flex space-x-20 animate-loop-scroll h-[70px]">
+          {/* <div className="flex overflow-hidden space-x-18 mt-4"> */}
+          <div className="flex space-x-20 h-[70px] logo-wrapper">
+            {/*  animate-loop-scroll  */}
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-bytedance.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 1"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-google.png"
+              height={50}
+              width={220}
+              unoptimized
+              className="max-w-none"
+              alt="Image 2"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-amazon.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 3"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-alibaba.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 4"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-tencent.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 5"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-neteast.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 6"
+            />
+            {/* </div> */}
+            {/* <div className="flex space-x-20 h-[70px] logo-wrapper"> */}
+            {/*  animate-loop-scroll  */}
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-bytedance.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 1"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-google.png"
+              height={50}
+              width={220}
+              unoptimized
+              className="max-w-none"
+              alt="Image 2"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-amazon.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 3"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-alibaba.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 4"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-tencent.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 5"
+            />
+            <Image
+              loading="lazy"
+              src="/images/logos/logo-neteast.png"
+              width={220}
+              height={50}
+              unoptimized
+              className="max-w-none"
+              alt="Image 6"
+            />
+            {/* </div> */}
+            {/* <div className="flex space-x-20 animate-loop-scroll h-[70px]">
               <Image
                 loading="lazy"
                 src="/images/logos/logo-bytedance.png"
@@ -62,63 +176,7 @@ const LogoSection = () => {
                 className="max-w-none"
                 alt="Image 6"
               />
-            </div>
-            <div className="flex space-x-20 animate-loop-scroll h-[70px]">
-              <Image
-                loading="lazy"
-                src="/images/logos/logo-bytedance.png"
-                width={300}
-                height={50}
-                unoptimized
-                className="max-w-none"
-                alt="Image 1"
-              />
-              <Image
-                loading="lazy"
-                src="/images/logos/logo-google.png"
-                height={50}
-                width={220}
-                unoptimized
-                className="max-w-none"
-                alt="Image 2"
-              />
-              <Image
-                loading="lazy"
-                src="/images/logos/logo-amazon.png"
-                width={220}
-                height={50}
-                unoptimized
-                className="max-w-none"
-                alt="Image 3"
-              />
-              <Image
-                loading="lazy"
-                src="/images/logos/logo-alibaba.png"
-                width={220}
-                height={50}
-                unoptimized
-                className="max-w-none"
-                alt="Image 4"
-              />
-              <Image
-                loading="lazy"
-                src="/images/logos/logo-tencent.png"
-                width={300}
-                height={50}
-                unoptimized
-                className="max-w-none"
-                alt="Image 5"
-              />
-              <Image
-                loading="lazy"
-                src="/images/logos/logo-neteast.png"
-                width={220}
-                height={50}
-                unoptimized
-                className="max-w-none"
-                alt="Image 6"
-              />
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
