1d0ca784bfb477eb5136c9076e4bff7de2e81ea8
jaythan <yegarrick12@gmail.com>
2024-08-01 14:49:59 +0800

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <>
       <ScrollToTopButton />
-      <main className="flex min-h-screen flex-col bg-[#121212]">
+      <main className="flex min-h-screen flex-col">
         <Navbar />
         {/* <div className="container mt-24 mx-auto px-12 py-4"> */}
         <HeaderSection />
