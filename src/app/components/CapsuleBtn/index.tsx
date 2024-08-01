9f30779d6be0c075eb9bc3f00c0a61ca8ed38719
jaythan <yegarrick12@gmail.com>
2024-08-01 15:46:54 +0800

    -

--- src/app/components/CapsuleBtn/index.tsx
+++ src/app/components/CapsuleBtn/index.tsx
@@ -9,7 +9,9 @@ const CapsuleBtn: NextPage<RootType> = ({ className = "" }) => {
   return (
     <div>
       <div className="overlap-capsule-button">
-        <button className="primary">sss</button>
+        <button className="primary">
+          <img className="mr-36" src="/images/icons/icon-3.png" />
+        </button>
         <button className="secondary">免费注册</button>
       </div>
     </div>
