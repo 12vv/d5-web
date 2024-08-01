37a274ad445579684ef0a54beb9f14644bb8c39f
jaythan <yegarrick12@gmail.com>
2024-08-01 14:25:47 +0800

    -

--- src/app/components/IconBtn/index.tsx
+++ src/app/components/IconBtn/index.tsx
@@ -8,7 +8,7 @@ const IconBtn = ({ icon, text, ...props }: any) => {
       className="icon-btn h-[60px] text-[18px] xl:h-[70px] xl:text-[22px] 2xl:h-[90px] 2xl:text-[30px]  2xl:px-8 flex items-center justify-center px-4 py-4 bg-[#D9D9D9] text-white rounded bg-opacity-30 rounded-lg"
     >
       {icon && <img src={icon} alt="icon" className="w-8 h-8 mr-4" />}
-      {text}
+      <span>{text}</span>
     </button>
   );
 };
