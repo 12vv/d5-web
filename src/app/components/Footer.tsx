8c22bd9d041cf9d48dc4d322d105d53c38ec9c27
jaythan <yegarrick12@gmail.com>
2024-09-10 11:14:59 +0800

    -

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="container mx-auto px-12 py-4">
-      <footer className="footer z-10 text-white">
-        <div className="container p-12 text-center">
-          <p className="font-semibold text-white">© 2024 d5</p>
-        </div>
+    <div className="container mx-auto px-12 py-4 border-t-[1px] border-[#7E7E7E] px-[10px]">
+      <footer className="footer z-10 text-white flex justify-between mb-[10px]">
+        <p className="font-semibold text-white">© 2024 d5</p>
+        <p className="font-semibold text-white">
+          <img src="/images/logos/3dlogo.png" className="w-[30px]" />
+        </p>
+
+        <p className="font-semibold text-white">© 2024 d5</p>
       </footer>
     </div>
   );
---
+++ src/app/components/RectBtn/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./index.css";
+
+interface RoundedButtonProps {
+  text: string;
+  onClick: () => void;
+  color?: string;
+  backgroundColor?: string;
+  backgroundImage?: string;
+  className?: string;
+  icon?: any;
+}
+
+const RectButton: React.FC<RoundedButtonProps> = ({
+  text,
+  onClick,
+  color,
+  backgroundColor,
+  backgroundImage,
+  className,
+  icon,
+}) => {
+  return (
+    <button
+      className={`rect-button glow-on-hover-2 ${className}`}
+      style={{
+        color: color || "white",
+        backgroundColor: backgroundColor || "",
+        backgroundImage: backgroundImage || "",
+      }}
+      onClick={onClick}
+    >
+      {icon && <img src={icon} alt="icon" className="w-6 h-6 mr-2" />}
+      {text}
+    </button>
+  );
+};
+
+export default RectButton;
